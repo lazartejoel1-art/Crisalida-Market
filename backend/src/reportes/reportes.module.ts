ff2889925bda47477ba0324ccf68c969853f85e2
lazartejoel-arte <lazartejoel1@gmail.com>
2026-04-26 16:00:47 -0400

    fix reportes module obra repository

--- backend/src/reportes/reportes.module.ts
+++ backend/src/reportes/reportes.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Pedido } from '../pedidos/pedidos.entity';
-import { ReportesService } from './reportes.service';
 import { ReportesController } from './reportes.controller';
+import { ReportesService } from './reportes.service';
+import { Pedido } from '../pedidos/pedidos.entity';
+import { Obra } from '../obras/obra.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Pedido])],
-  providers: [ReportesService],
+  imports: [TypeOrmModule.forFeature([Pedido, Obra])],
   controllers: [ReportesController],
+  providers: [ReportesService],
 })
 export class ReportesModule {}
